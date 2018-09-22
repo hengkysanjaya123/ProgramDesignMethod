0f51a862ed69d1f474e3f93e466a15dc72da9ca7
Hengky Sanjaya <hengkysanjaya204@gmail.com>
2018-09-22 22:39:06 +0700

    add validation to driving simulation

--- drivingsimulation.py
+++ drivingsimulation.py
@@ -1,24 +1,42 @@
-initial_velocity = 0
-time_spent = int(input("Please input Time​ ​spent​ ​on​ ​the​ ​road​ :"))
-acceleration = int(input("Please input Acceleration : "))
-distance = int(input("Please input distance : "))
-speed_limit = 60
+while True:
+    try:
+        time_spent = int(input("Please input Time​ ​spent​ ​on​ ​the​ ​road​ :"))
+    except ValueError:
+        print("Warning : Please input time spent correct format ( number )")
+        continue
 
-v = 0
-s = 0
-for i in range(0, time_spent + 1):
-    v = initial_velocity + acceleration * i
-    s = initial_velocity * i + 1/2 * acceleration* i * i
-    print("Duration: "+str(i)+ " Distance : " + "*" * int(s /10))
+    try:
+        acceleration = int(input("Please input Acceleration : "))
+    except ValueError:
+        print("Warning : Please input acceleration correct format ( number )")
+        continue
+
+    try:
+        distance = int(input("Please input distance : "))
+    except ValueError:
+        print("Warning : Please input distance correct format ( number )")
+        continue
+
+    initial_velocity = 0
+    speed_limit = 60
+
+    v = 0
+    s = 0
+    for i in range(0, time_spent + 1):
+        v = initial_velocity + acceleration * i
+        s = initial_velocity * i + 1/2 * acceleration* i * i
+        print("Duration: "+str(i)+ " Distance : " + "*" * int(s /10))
 
 
-if (v > speed_limit):
-    print("The​ ​person​ ​went​ ​over​ ​the​ ​speed​ ​limit.​ ​(Max​ ​speed​ ​was​ ​"+str(v)+" ​m/s)")
-else:
-    print("The​ ​person​ ​did​ ​not​ ​go​ ​over​ ​the​ ​speed​ ​limit.​ ​(Max​ ​speed​ ​was​ "+str(v)+" ​m/s) ")
+    if (v > speed_limit):
+        print("The​ ​person​ ​went​ ​over​ ​the​ ​speed​ ​limit.​ ​(Max​ ​speed​ ​was​ ​"+str(v)+" ​m/s)")
+    else:
+        print("The​ ​person​ ​did​ ​not​ ​go​ ​over​ ​the​ ​speed​ ​limit.​ ​(Max​ ​speed​ ​was​ "+str(v)+" ​m/s) ")
 
 
-if(s >= distance):
-    print("The​ ​person​ ​reached​ ​the​ ​destination.​ ​(Reached​ ​"+str(s)+"m)")
-else:
-    print("The​ ​person​ ​did​ ​not​ ​reach​ ​the​ ​destination.​ ​(Reached​ "+str(s)+" m) ")
+    if(s >= distance):
+        print("The​ ​person​ ​reached​ ​the​ ​destination.​ ​(Reached​ ​"+str(s)+"m)")
+    else:
+        print("The​ ​person​ ​did​ ​not​ ​reach​ ​the​ ​destination.​ ​(Reached​ "+str(s)+" m) ")
+
+    break
